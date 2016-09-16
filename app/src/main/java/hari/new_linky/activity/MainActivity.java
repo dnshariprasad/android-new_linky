b83c7fa4885b265f1073ea83da48bee5d174472a
ramesh <hari.kvana@gmail.com>
2016-09-16 14:39:26 +0530

    signin cleanup

--- app/src/main/java/hari/new_linky/activity/MainActivity.java
+++ app/src/main/java/hari/new_linky/activity/MainActivity.java
@@ -1,54 +1,21 @@
 package hari.new_linky.activity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 
 import hari.new_linky.R;
-import hari.new_linky.network.NetworkHandler;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private static final String TAG = "MainActivity";
-    private Button btnSignin;
-    private EditText etEmail, etPassword;
+public class MainActivity extends AppCompatActivity {
+    public static void start(Context context) {
+        Intent starter = new Intent(context, MainActivity.class);
+        context.startActivity(starter);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        etEmail = (EditText) findViewById(R.id.et_email);
-        etPassword = (EditText) findViewById(R.id.et_password);
-
-        btnSignin = (Button) findViewById(R.id.btn_signin);
-        btnSignin.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.btn_signin:
-                NetworkHandler.getInstance().signin("a@m.com", "abc").enqueue(new Callback<String>() {
-                    @Override
-                    public void onResponse(Call<String> call, Response<String> response) {
-                        if (response.isSuccessful()) {
-                            Log.d(TAG, "onResponse: " + response.raw());
-                        } else {
-                            Log.e(TAG, "onResponse: " + response.raw());
-                        }
-                    }
-
-                    @Override
-                    public void onFailure(Call<String> call, Throwable t) {
-                        Log.e(TAG, "onFailure: " + t.getLocalizedMessage());
-                    }
-                });
-                break;
-        }
     }
 }
