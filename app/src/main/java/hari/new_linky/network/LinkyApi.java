10398818720b610e6a438ccc92a875bf03a0c1b4
ramesh <hari.kvana@gmail.com>
2016-09-16 16:45:15 +0530

    signup implemented

--- app/src/main/java/hari/new_linky/network/LinkyApi.java
+++ app/src/main/java/hari/new_linky/network/LinkyApi.java
@@ -1,6 +1,8 @@
 package hari.new_linky.network;
 
+import hari.new_linky.model.SignUpInput;
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.POST;
@@ -12,4 +14,7 @@ public interface LinkyApi {
     @FormUrlEncoded
     @POST("/signin.json")
     Call<String> signin(@Field("email") String email, @Field("password") String password);
+
+    @POST("/users.json")
+    Call<String> signup(@Body SignUpInput signUpInput);
 }
